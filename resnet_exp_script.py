f7250d785b0515ee78f5d2bafde4ec66a5830568
Yiming Zhou <yimingzhou0416@gmail.com>
2020-04-19 16:10:16 -0700

    initial commit

---
+++ resnet_exp_script.py
@@ -0,0 +1,14 @@
+import os
+
+os.system('python main.py --training_noise 0.05 --device 0 --test_with_mean --tensorboard')
+
+os.system('python main.py --training_noise 0.05 --training_noise_mean 0.001 --device 0 --test_with_mean --tensorboard')
+os.system('python main.py --training_noise 0.05 --training_noise_mean 0.002 --device 0 --test_with_mean --tensorboard')
+os.system('python main.py --training_noise 0.05 --training_noise_mean 0.003 --device 0 --test_with_mean --tensorboard')
+os.system('python main.py --training_noise 0.05 --training_noise_mean 0.004 --device 0 --test_with_mean --tensorboard')
+os.system('python main.py --training_noise 0.05 --training_noise_mean 0.005 --device 0 --test_with_mean --tensorboard')
+os.system('python main.py --training_noise 0.05 --training_noise_mean -0.001 --device 0 --test_with_mean --tensorboard')
+os.system('python main.py --training_noise 0.05 --training_noise_mean -0.002 --device 0 --test_with_mean --tensorboard')
+os.system('python main.py --training_noise 0.05 --training_noise_mean -0.003 --device 0 --test_with_mean --tensorboard')
+os.system('python main.py --training_noise 0.05 --training_noise_mean -0.004 --device 0 --test_with_mean --tensorboard')
+os.system('python main.py --training_noise 0.05 --training_noise_mean -0.005 --device 0 --test_with_mean --tensorboard')
